8bc715d4437eb5af7754c83084d014ed04039520
Pardeep kaur <Pardeeptwb@gmail.com>
2021-03-11 21:27:50 -0500

    commit

--- purchase form.js
+++ purchase form.js
@@ -1,37 +1,4 @@
-var owner = $('#owner'),
-    cardNumber = $('#cardNumber'),
-    cardNumberField = $('#card-number-field'),
-    CVV = $("#cvv"),
-    mastercard = $("#mastercard"),
-    confirmButton = $('#confirm-purchase'),
-    visa = $("#visa"),
-    amex = $("#amex");
-    cardNumber.payform('formatCardNumber');
-    CVV.payform('formatCardCVC');cardNumber.keyup(function() {
-        amex.removeClass('transparent');
-        visa.removeClass('transparent');
-        mastercard.removeClass('transparent');
-    
-        if ($.payform.validateCardNumber(cardNumber.val()) == false) {
-            cardNumberField.removeClass('has-success');
-            cardNumberField.addClass('has-error');
-        } else {
-            cardNumberField.removeClass('has-error');
-            cardNumberField.addClass('has-success');
-        }
-    
-        if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
-            mastercard.addClass('transparent');
-            amex.addClass('transparent');
-        } else if ($.payform.parseCardType(cardNumber.val()) == 'amex') {
-            mastercard.addClass('transparent');
-            visa.addClass('transparent');
-        } else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
-            amex.addClass('transparent');
-            visa.addClass('transparent');
-        }
-    });
-  
+
     confirmButton.click(function(e) {
         e.preventDefault();
     
